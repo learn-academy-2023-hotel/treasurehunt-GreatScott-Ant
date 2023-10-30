c4985ea4f5a3aa3134d0fe78010da6661c058d41
anthonyholt <ant.scott227@gmail.com>
2023-10-30 16:24:25 -0700

    last treasure

--- src/App.js
+++ src/App.js
@@ -21,30 +21,57 @@ const App = () => {
     const[bombLocation, setBombLocation]= useState(
       Math.floor(Math.random() * board.length)
       )
+      
+      
+      
+      const handleSquareClick = (clickedSquareIndex) => {
+        //create a variable holding copy of current state
+        let updatedBoard = [... board]
+        // set condition for if treausre location is same as clicked square's index show a treasure
+        if(clickedSquareIndex === treasureLocation) {
+          // then reassign state value at that index to treasure emoji
+          updatedBoard[clickedSquareIndex] = "💎"
+          alert ("You've Found the Treasure!")
+          // setBoard(updatedBoard)
+        } else if (clickedSquareIndex === bombLocation) {
+          updatedBoard[clickedSquareIndex] = "💣"
+          alert ("GAME OVER!")
+          // setBoard(updatedBoard)
+        } else {
+          //  use index from clickedSquareIndex to update the current square's value with emoji
+          updatedBoard[clickedSquareIndex] = "🌴"
+          setAttempts(attempts -1)
+          //  update state with the new board
+          
+        }
+        setBoard(updatedBoard)
+        
+      }
+      
+      const reset = ()=> {
+        // const gameReset = board.map () => "?" {
+        // setBoard(resetBoard)
+        window.location.reload()
 
-  const handleSquareClick = (clickedSquareIndex) => {
-    //create a variable holding copy of current state
-    let updatedBoard = [... board]
-    // set condition for if treausre location is same as clicked square's index show a treasure
-    if(clickedSquareIndex === treasureLocation) {
-      // then reassign state value at that index to treasure emoji
-    updatedBoard[clickedSquareIndex] = "💎"
-    // setBoard(updatedBoard)
-    } else if (clickedSquareIndex === bombLocation) {
-      updatedBoard[clickedSquareIndex] = "💣"
-      // setBoard(updatedBoard)
-    } else {
-    //  use index from clickedSquareIndex to update the current square's value with emoji
-   updatedBoard[clickedSquareIndex] = "🌴"
-  //  update state with the new board
-  
-}
-  setBoard(updatedBoard)
-}      
+        
+      }
 
-  return (
-    <>
-      <h1>Treasure Hunt Game</h1>
+      const[attempts, setAttempts] = useState(5)
+
+      // function handleWin(e) {
+      //     if(e.target.className== "treasureLocation") {
+      //       e.stopPropagation();
+      //     }
+
+        // function handleWin(e) {
+        //   if(e.target.className=="💣") {
+        //     e.stopPropagation();
+        //   }
+      
+      return (
+        <>
+      <h1>National Treasure III</h1>
+
       <div className = "board">
       {/* map over array and return a square for each element*/}
       {board.map((value, index) => {
@@ -54,10 +81,21 @@ const App = () => {
       index={index} 
       handleSquareClick={handleSquareClick}
       />
+
     })}
     </div>
+
+      <div className = "centerButton">
+      <button onClick={reset}>
+            Play Again
+          </button>
+
+      <div classname="attemptbox">Attempts Remaining: {attempts}
+            </div>
+      </div>
     </>
   )
 }
+
 
 export default App
